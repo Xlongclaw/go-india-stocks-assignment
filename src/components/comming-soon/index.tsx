a01d1047a7cb5906a7d7f11e4b3cdae5e40e476d
unknown <xlongclaw@gmail.com>
2024-03-21 12:51:56 +0530

    organised code for comming soon

--- src/components/comming-soon/index.tsx
+++ src/components/comming-soon/index.tsx
@@ -1,18 +1,34 @@
-import React from 'react'
+/**
+ * @file CommingSoon.tsx
+ * @description A component representing a "Coming Soon" section.
+ */
 
+import React from 'react';
+
+/**
+ * Props interface for CommingSoon component.
+ */
 type PropsType = {
-  children:React.JSX.Element,
-  title:string
+  children: React.JSX.Element; // Child elements
+  title: string; // Title for the "Coming Soon" section
 }
 
-const CommingSoon:React.FC<PropsType> = (props) => {
+/**
+ * CommingSoon component represents a "Coming Soon" section.
+ * @param props - Props for the CommingSoon component.
+ * @returns - JSX element representing the CommingSoon component.
+ */
+const CommingSoon: React.FC<PropsType> = (props) => {
   return (
     <div className='flex flex-col justify-center items-center w-full poppins h-full'>
+      {/* Child elements */}
       {props.children}
+      {/* Title */}
       <h3 className='text-3xl'>{props.title}</h3>
-      <h3 className='text-sm tracking-[5px]'>COMMING SOON</h3>
+      {/* "Coming Soon" text */}
+      <h3 className='text-sm tracking-[5px]'>COMING SOON</h3>
     </div>
-  )
+  );
 }
 
-export default CommingSoon
+export default CommingSoon;
