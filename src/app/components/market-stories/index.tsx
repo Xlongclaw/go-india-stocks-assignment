5602a9945a92acd2e69fef3b5c8e3ce1e10dbd2e
unknown <xlongclaw@gmail.com>
2024-03-21 12:50:42 +0530

    organised code for market stories

--- src/app/components/market-stories/index.tsx
+++ src/app/components/market-stories/index.tsx
@@ -1,14 +1,20 @@
-import ImageCarousel from "@/components/image-carousel";
+/**
+ * @file MarketStories.tsx
+ * @description A component representing market stories with a list of stories.
+ */
+
 import React from "react";
 import StoriesWrapper from "./stories-wapper";
 import MARKET_STORIES from "@/constants/MARKET_STORIES";
 
+/**
+ * MarketStories component represents market stories with a list of stories.
+ * @returns - JSX element representing the MarketStories component.
+ */
 const MarketStories = () => {
   return (
     <div className="px-[6px] w-[30rem] overflow-y-auto x-scroll hidden xl:block">
-      {/* <h3 className="font-bold text-x-accent-base oswald text-sm my-4 text-center">
-        MARKET STORIES
-      </h3> */}
+      {/* Market stories list */}
       {MARKET_STORIES.map((story) => (
         <StoriesWrapper
           key={story.title}
--- src/app/components/market-stories/stories-wapper/index.tsx
+++ src/app/components/market-stories/stories-wapper/index.tsx
@@ -1,17 +1,28 @@
-import { MarketStoriesType } from "@/types";
+/**
+ * @file StoriesWrapper.tsx
+ * @description A component representing a wrapper for market stories.
+ */
+
+import { MarketStoriesType } from "@/types"; // Importing MarketStoriesType from types
 import Image from "next/image";
 import React from "react";
 
-type PropsType = MarketStoriesType;
-
-const StoriesWrapper: React.FC<PropsType> = (props) => {
+/**
+ * StoriesWrapper component represents a wrapper for market stories.
+ * @param props - Props for the StoriesWrapper component.
+ * @returns - JSX element representing the StoriesWrapper component.
+ */
+const StoriesWrapper: React.FC<MarketStoriesType> = (props) => {
   return (
-    <div className=" bg-white rounded-2xl shadow6 poppins overflow-hidden my-4">
-      <Image src={props.image} width={500}
-            height={500} alt="" />
+    <div className="bg-white rounded-2xl shadow6 poppins overflow-hidden my-4">
+      {/* Story image */}
+      <Image src={props.image} width={500} height={500} alt="" />
       <div className="p-5">
+        {/* Story title */}
         <h3 className="font-bold text-sm text-x-accent-base">{props.title}</h3>
+        {/* Story date */}
         <h5 className="text-xs text-x-sky-blue font-bold mt-2">{props.date}</h5>
+        {/* Story short description */}
         <p className="text-xs font-medium text-x-accent-light/80 mt-2">
           {props.shortDes}
         </p>
